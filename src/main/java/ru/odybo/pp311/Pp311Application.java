77759a79000d8ae44630c6c8723b9e61f72ad964
Sergey Ryabinin <ryabinin.sergey@gmail.com>
2022-10-25 01:30:31 +0300

    start spring boot app

---
+++ src/main/java/ru/odybo/pp311/Pp311Application.java
@@ -0,0 +1,13 @@
+package ru.odybo.pp311;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp311Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp311Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/odybo/pp311/Pp311ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.odybo.pp311;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp311ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
